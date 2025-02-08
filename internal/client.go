0cc02e440586d272f34f0b284f3b33e53b6aa20b
Matt Krueger <mkrueger@rstms.net>
2025-02-08 09:24:12 -0700

    remove printf; change SRC lookup to add 's' in resolver command, so URL will be correct

--- internal/client.go
+++ internal/client.go
@@ -21,8 +21,7 @@ func DiscoverContextURL(ctx context.Context, service, domain string) (string, er
 	var resolver net.Resolver
 
 	// Only lookup TLS records, plaintext connections are insecure
-	fmt.Printf("DiscoverContextURL: lookup=%s\n", service)
-	_, addrs, err := resolver.LookupSRV(ctx, service, "tcp", domain)
+	_, addrs, err := resolver.LookupSRV(ctx, service+"s", "tcp", domain)
 	if dnsErr, ok := err.(*net.DNSError); ok {
 		if dnsErr.IsTemporary {
 			return "", err
